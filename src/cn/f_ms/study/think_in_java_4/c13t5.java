9fd9fd8fd04202152287b22850d8bb45b7da769a
f_ms <imf_ms@yeah.net>
2018-07-09 20:55:07 +0800

    add c13t5

---
+++ src/cn/f_ms/study/think_in_java_4/c13t5.java
@@ -0,0 +1,27 @@
+package cn.f_ms.study.think_in_java_4;
+
+import java.util.regex.Pattern;
+
+class c13t5 {
+
+    private static final Pattern BEGIN_UPPER_END_DOT_REGEX = Pattern.compile("[A-Z].+\\.");
+
+    public static void main(String[] args) {
+
+        String[] strings = {
+                "hello world",
+                "Hello world",
+                "Hello world.",
+                "Are You OK?",
+                "Are you OK."
+        };
+
+        for (String string : strings) {
+            System.out.println(String.format(
+                    "%s: %b",
+                    string,
+                    BEGIN_UPPER_END_DOT_REGEX.matcher(string).matches()
+            ));
+        }
+    }
+}
