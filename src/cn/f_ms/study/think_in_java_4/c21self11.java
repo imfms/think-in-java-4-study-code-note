b085f6319467f513fcaf364c5fedffcabd7945e7
f_ms <imf_ms@yeah.net>
2018-11-27 19:26:37 +0800

    add c21self11

---
+++ src/cn/f_ms/study/think_in_java_4/c21self11.java
@@ -0,0 +1,75 @@
+package cn.f_ms.study.think_in_java_4;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+class c21self11 {
+
+    public static class SerialNumberGenerator {
+        private static volatile AtomicInteger serialNumber = new AtomicInteger(0);
+        public static int nextSerialNumber() {
+            return serialNumber.getAndAdd(1);
+        }
+    }
+
+    // Reuses storage so we don't run out of memory:
+    static class CircularSet {
+        private int[] array;
+        private int len;
+        private int index = 0;
+
+        public CircularSet(int size) {
+            array = new int[size];
+            len = size;
+            // Initialize to a value not produced
+            // by the SerialNumberGenerator:
+            for (int i = 0; i < size; i++)
+                array[i] = -1;
+        }
+
+        public synchronized void add(int i) {
+            array[index] = i;
+            // Wrap index and write over old elements:
+            index = ++index % len;
+        }
+
+        public synchronized boolean contains(int val) {
+            for (int i = 0; i < len; i++)
+                if (array[i] == val) return true;
+            return false;
+        }
+    }
+
+    private static final int SIZE = 10;
+    private static CircularSet serials =
+            new CircularSet(1000);
+    private static ExecutorService exec =
+            Executors.newCachedThreadPool();
+
+    static class SerialChecker implements Runnable {
+        public void run() {
+            while (true) {
+                int serial =
+                        SerialNumberGenerator.nextSerialNumber();
+                if (serials.contains(serial)) {
+                    System.out.println("Duplicate: " + serial);
+                    System.exit(0);
+                }
+                serials.add(serial);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < SIZE; i++)
+            exec.execute(new SerialChecker());
+        // Stop after n seconds if there's an argument:
+        if (args.length > 0) {
+            TimeUnit.SECONDS.sleep(new Integer(args[0]));
+            System.out.println("No duplicates detected");
+            System.exit(0);
+        }
+    }
+}
